233cf1967b8421bf7ec9a6e6b03c961eb6939017
mixfolio <62788627+mixfolio@users.noreply.github.com>
2020-11-27 11:28:19 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+
+   @@include("burger.js")
+   @@include("sidebar-right.js")
+   @@include("current-date.js")
+   @@include("tabs.js")
+   @@include("form.js")
+   //@@include("exit-log.js")
+
+   //@@include("spend.js")
+
+   
+
+   // Header-avatar
+   // const headerAvatar = document.querySelector('.header-avatar'),
+   //    headerAvatarText = headerAvatar.querySelector('.header-avatar__text');
+   // headerAvatar.addEventListener('click', () => {
+   //    headerAvatarText.classList.toggle('hide');
+   // });
+
+});
